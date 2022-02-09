4d703d7382b9e018dfe7fc2de049ba2f514f6e65
gusmontoya <gus.montoya@gmail.com>
2022-02-09 11:31:52 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Intro
+
+This repo is a simple TODO list. It is much simplified from the original (and not working) I created during a technical challenge.
+
+It uses HTML, CSS and JavaScript, and using the same layout and color scheme as the technical challenge. This application is sized for non-mobile devices.
+
+Use
+Add an item:
+- To add an item, type a ToDo in the input field and click on the plus sign. 
+
+Indicate an item is done:
+- To indicate an item is done, click once on the listed item and a line with strike though it. 
+
+Remove an item:
+- To remove an item from the list, just double click on it. 
+
+The site is deployed on Netlify. VIEW DEMO HERE!
+
+2022 © Gus Montoya
---
+++ js/script.js
@@ -0,0 +1,30 @@
+// 
+let addToDoList = document.getElementById('addToDo'); 
+let userInputField = document.getElementById('inputField');
+let todoList = document.getElementById('toDoContainer');
+
+
+// the Event listener will "listen" for a click of the mouse 
+// and run a function that will create an element <li>. This 
+// will allow the creation of a new list in the TODO-List. 
+addToDoList.addEventListener('click', function() {
+    let list = document.createElement('li');
+    // this add's the styling for each list added 
+    list.classList.add('li');
+    // specifies the value of the input to the list being created
+    list.innerText = userInputField.value;
+    // In order to add the created <li> element to the List it needs to be 
+    // to be appended.
+    todoList.appendChild(list);
+    // this resets the input to empty after new list is added
+    userInputField.value = "";
+       // this uses an event listener "click" to change the listed item 
+    // to strikethrough 
+    list.addEventListener('click', function(){
+        list.style.textDecoration = "line-through";
+    })
+    // this removes the listed item with a double click
+    list.addEventListener('dblclick', function(){
+        todoList.removeChild(list);
+    })
+})
